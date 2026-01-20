b34cdd43fee8baa7350f6e05ba08c41cc9a27923
shiyanhui <lime.syh@gmail.com>
2026-01-20 09:37:23 +0800

    Update to v0.0.1

--- Casks/coni.rb
+++ Casks/coni.rb
@@ -2,12 +2,12 @@ cask "coni" do
   version "0.0.1"
 
   on_arm do
-    sha256 "bb34e1a586398c1105468b62e7a098c52f2754c9d16e05b5fb19fc50f1fb75fb"
+    sha256 "b706e57aa567d1f89d61c375daf95553455c6b797507f45e470ce9e05dc04f48"
     url "https://github.com/coni-ai/coni/releases/download/v#{version}/Coni-#{version}-arm64.dmg"
   end
 
   on_intel do
-    sha256 "77a18dbd1b973281be8a31e44f6d50a2cf863ebd73fb5b1162b73eb496d81e1b"
+    sha256 "54ab8704a6c2d144e168197c0b64799accdb829d0dacdf8c2f2c77befd252cd0"
     url "https://github.com/coni-ai/coni/releases/download/v#{version}/Coni-#{version}.dmg"
   end
 
